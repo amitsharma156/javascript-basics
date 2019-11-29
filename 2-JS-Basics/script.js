1747da5016db87156f56974bd2564eb0bf50d1a9
Amit <sharma88.amit@gmail.com>
2019-11-29 20:19:07 +0530

    javascript basics operators

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -18,28 +18,76 @@ var function =23;
 */
 /********************
  variable mutation  and type Coercion */
- var firstName ='John';
- var age =28 ;
+//  var firstName ='John';
+//  var age =28 ;
 
- //type coerction
- console.log( age+ ' ' + firstName);
+//  //type coerction
+//  console.log( age+ ' ' + firstName);
 
- var job, isMarried;
+//  var job, isMarried;
 
- job ='teacher';
- isMarried = false;
+//  job ='teacher';
+//  isMarried = false;
 
- console.log(firstName + ' is a  ' +age+' years old '+ job + '. Is he married : '+ isMarried );
+//  console.log(firstName + ' is a  ' +age+' years old '+ job + '. Is he married : '+ isMarried );
 
-// Type Mutation
+// // Type Mutation
 
-age = 'twenty eight';
-job = 'driver';
-middleName = 'steve'
-alert(firstName ++' '+ middleName+ ' is a  ' +age+' years old '+ job + '. Is he married : '+ isMarried );
+// age = 'twenty eight';
+// job = 'driver';
+// middleName = 'steve'
+// alert(firstName ++' '+ middleName+ ' is a  ' +age+' years old '+ job + '. Is he married : '+ isMarried );
 
-var lastName = prompt('What is his last name');
-console.log(firstName+ ' ' +lastName);
+// var lastName = prompt('What is his last name');
+// console.log(firstName+ ' ' +lastName);
+
+/* **********BAsic Operators ***************/
+
+var year, yearJohn, yearMark, ageJohn, ageMark;
+ageJohn =28;
+ageMark = 33;
+now = 2018;
+yearJohn = now-ageJohn;
+yearMark = now-ageMark;
+
+console.log(yearJohn);
+console.log(yearMark);
+console.log(now+2);
+console.log(now*2);
+console.log(now/2);
+
+//Logical operators
+
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+
+console.log(typeof(johnOlder));
+console.log(typeof ageJohn);
+
+console.log(typeof "ageJohn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
