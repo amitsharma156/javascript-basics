d1b17994c96c5e35a3e38162d6087893a85cfbec
Amit <sharma88.amit@gmail.com>
2019-11-29 23:53:05 +0530

    javascript basics function

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -251,6 +251,40 @@ else {
     console.log('there is a tie');
 }
 
+/*********  Functions   ************/
+
+function calculateAge(birthYear){
+    return 2018-birthYear;
+}
+var ageJohn = calculateAge(1988);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn , ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName){
+    var age = calculateAge(year);
+    var retirement = 65-age;
+    if(retirement>0){
+    console.log(firstName + ' retires in ' + retirement + ' years');
+    }
+    else{
+        console.log('already retired');
+    }
+}
+yearsUntilRetirement(1990,'John');
+yearsUntilRetirement(1948,'Mike');
+yearsUntilRetirement(1969,'Jane');
+
+
+
+
+
+
+
+
+
+
+
 
 
 
