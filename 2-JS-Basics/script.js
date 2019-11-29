a3b17bff2e9fba6cdc5dcc225a2877f193e3ad49
Amit <sharma88.amit@gmail.com>
2019-11-29 19:54:08 +0530

    javascript basics variable mutation and coercion

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,3 +1,5 @@
+/* variables
+    declaration
 var firstName = 'John';
 console.log(firstName);
 
@@ -6,3 +8,76 @@ var age = 28;
 
 var fullAge = true;
 console.log(fullAge);
+
+var job;
+job = 'teacher';
+console.log(job);
+
+// Variables naming rules
+var function =23;
+*/
+/********************
+ variable mutation  and type Coercion */
+ var firstName ='John';
+ var age =28 ;
+
+ //type coerction
+ console.log( age+ ' ' + firstName);
+
+ var job, isMarried;
+
+ job ='teacher';
+ isMarried = false;
+
+ console.log(firstName + ' is a  ' +age+' years old '+ job + '. Is he married : '+ isMarried );
+
+// Type Mutation
+
+age = 'twenty eight';
+job = 'driver';
+middleName = 'steve'
+alert(firstName ++' '+ middleName+ ' is a  ' +age+' years old '+ job + '. Is he married : '+ isMarried );
+
+var lastName = prompt('What is his last name');
+console.log(firstName+ ' ' +lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
