acb722bd7b553b57a4db4485cabb9e95823bfc4c
Amit <sharma88.amit@gmail.com>
2019-11-30 12:42:12 +0530

    Javascript Basics Objects and methods

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -400,6 +400,42 @@ jane.birthYear = 1990;
 jane['lastName'] = 'Smith';
 console.log(jane);
 
+/************* Objects and methods *************/
+
+var john = {
+    firstName : 'John',
+    lastName : 'Smith',
+    birthYear : 1992,
+    family : ['Jane','Mark','Bob','Emily'],
+    job : 'teacher',
+    isMarried : false,
+    calcAge : function(){
+        this.age = 2018-this.birthYear;
+        
+    }
+};
+john.calcAge();
+console.log(john);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
